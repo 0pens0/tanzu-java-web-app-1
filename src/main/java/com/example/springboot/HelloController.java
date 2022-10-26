39d5d4a7dc6cb973c1b6db6a552392584d2e3c14
Antonio Gallego <agallego@vmware.com>
2022-10-26 13:27:29 +0200

    v2

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Running on TAP 1.3, and this is awesome! Hello!";
+		return "Greetings from Spring Boot + Tanzu! Running on TAP 1.3, and this is awesome! Hello! v2";
 	}
 
 }
