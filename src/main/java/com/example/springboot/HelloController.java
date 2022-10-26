5517f7276aec4b4da5bcb337e2aca98aebd00845
ages <agallego@vmware.com>
2022-10-26 02:12:17 +0200

    update in msg

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "Greetings from Spring Boot + Tanzu! Running on TAP 1.3";
 	}
 
 }
